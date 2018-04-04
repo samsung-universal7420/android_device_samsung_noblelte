076733ba2091bb71ceec0c5da635277e96c218bd
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    noblelte: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -19,3 +19,7 @@ LOCAL_PATH := device/samsung/noblelte
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_noblelte.mk
 
+COMMON_LUNCH_CHOICES := \
+    lineage_noblelte-user \
+    lineage_noblelte-userdebug \
+    lineage_noblelte-eng
